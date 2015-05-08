55e73dce56e3d1511bbe4e339e399101bd9b1061
pozdny <pozdny@mail.ru>
2015-05-08 13:54:24 +0700

    Last change

---
+++ mail/confirmEmail.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Valentina
+ * Date: 29.01.2015
+ * Time: 16:38
+ */
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $user app\modules\user\models\User */
+
+$confirmLink = Yii::$app->urlManager->createAbsoluteUrl(['user/default/confirm-email', 'token' => $user->email_confirm_token]);
+?>
+
+Здравствуйте, <?= Html::encode($user->username) ?>!
+
+Для подтверждения адреса пройдите по ссылке:
+
+<?= Html::a(Html::encode($confirmLink), $confirmLink) ?>
+
+Если Вы не регистрировались у на нашем сайте, то просто удалите это письмо.
---
+++ modules/main/controllers/ContactController.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Valentina
+ * Date: 06.05.2015
+ * Time: 20:43
+ */ 
